1d98907a7e676bb7f382bf24675d52200a87ce9f
DESKTOP-9KDRKT9\rizwa_hrm9oc0 <rizwan.bsse@gmail.com>
2023-11-07 22:19:14 +0500

    additional test cases and fome optimization

--- src/tests/stockService.test.ts
+++ src/tests/stockService.test.ts
@@ -1,9 +1,22 @@
 import { getCurrentStock } from '../services/stockService';
 
-// Example test using Jest
+describe('getStockLevel', () => {
 test('get current stock for existing SKU', async () => {
-  const sku = 'LTV719449/39/39';
+  const sku = 'LTV719449/39/39'; // hardcoded an 
   // You would use mocks to simulate file reading here
   const stock = await getCurrentStock(sku);
-  expect(stock.stock).toBeGreaterThan(0);
+  expect(stock.sku).toBe(sku);
+  
+  // Check that the stock (quantity) property is as any number
+  expect(typeof stock.stock).toBe("number"); 
 });
+
+test('test for non existing sku in stock and transaction', async () => {
+    const sku = 'NOT/FOUND/SKU';
+    // You would use mocks to simulate file reading here
+    expect.objectContaining({
+        statusCode: 404,
+        message: `SKU ${sku} does not exist.`
+      })
+  });
+})
